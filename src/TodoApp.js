639f3a6821fcbe643e9536358397dc05b748cacb
Yosri-zayani <zayaniyosri20@gmail.com>
2023-05-12 21:52:24 +0100

    updated version of TODO app

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,25 +1,30 @@
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./TodoApp.css";
+import backgroundImage from "./images/bg-desktop-dark.jpeg";
 
 const TodoApp = () => {
   const [todos, setTodos] = useState([]);
   const [inputValue, setInputValue] = useState([]);
   const [deletedTodos, setDeletedTodos] = useState([]);
   const [showArchive, setShowArchive] = useState(false);
-
+  const [hideArchive, SetHideArchive] = useState(true);
+  const Cond = hideArchive || deletedTodos.length === 0;
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
   };
 
   const handleAddTodo = () => {
-    if (inputValue.trim() !== '') {
+    if (inputValue.trim() !== "") {
       const newTodo = {
         id: new Date().getTime(),
         text: inputValue,
         active: false,
       };
+      SetHideArchive(false);
+
       setTodos([...todos, newTodo]);
-      setInputValue('');
+      setInputValue("");
     }
   };
 
@@ -45,70 +50,92 @@ const TodoApp = () => {
 
   const handleDeleteArchive = () => {
     setDeletedTodos([]);
-    
-  }    
+  };
   const handleShowArchive = () => {
     setShowArchive(!showArchive);
   };
 
   return (
-    <div className="container mt-4">
-      <h1 className="text-center">Todo App</h1>
-      <div className="d-flex">
-        <input
-          type="text"
-          className="form-control mr-2"
-          placeholder="Enter a todo"
-          value={inputValue}
-          onChange={handleInputChange}
-        />
-        <button className="btn btn-primary" onClick={handleAddTodo}>
-          Add Todo
-        </button>
-      </div>
-      <ul className="list-group mt-4">
-        {todos.map((todo) => (
-          <li
-            className={`list-group-item d-flex justify-content-between ${
-              todo.active ? 'active' : ''
-            }`}
-            key={todo.id}
-          >
-            <div>
-              <input
-                type="checkbox"
-                checked={todo.active}
-                onChange={() => handleToggleActive(todo.id)}
-                className="mr-2"
-              />
-              {todo.text}
-            </div>
-            <button className="btn btn-danger" onClick={() => handleDeleteTodo(todo.id)}>
-              Delete
+    <>
+      <div className="overflow-auto page-container">
+        <img className="image" src={backgroundImage} alt="background"></img>
+        <center>
+        <div className="container-sm "style={{marginTop:"12rem",maxHeight:'80%'}}>
+          <h1 className="Title text-left">TODO </h1>
+          <div className="container-md d-flex" >
+            <input
+              type="text"
+              className="inputField form-control mr-2"
+              placeholder="Enter task here "
+              value={inputValue}
+              onChange={handleInputChange}
+            />
+            <button className="btn btn-primary" onClick={handleAddTodo}>
+              Add Todo
             </button>
-          </li>
-        ))}
-      </ul>
-      <div className="footer mt-4">
-        <button className="btn btn-secondary" onClick={handleShowArchive}>
-          {showArchive ? 'Hide Archive' : 'Show Archive'}
-        </button>
-        {showArchive && (
-          <div className="archive">
-            <h2>Archive</h2>
-            <ul className="list-group mt-4">
-              {deletedTodos.map((todo) => (
-                <li className="list-group-item" key={todo.id}>
-                  {todo.text}
-                </li>
-              ))
-            }
-            </ul>
-            <button className="btn btn-secondary" onClick={handleDeleteArchive}>Delete archive</button>
           </div>
-        )}
+          <div className="container  center mt-4">
+            <ul className="list-group mt-4">
+              {todos.map((todo) => (
+                <li
+                  className={`inputField list-group-item d-flex justify-content-between ${
+                    todo.active ? "active" : ""
+                  }`}
+                  key={todo.id}
+                >
+                  <div>
+                    <input
+                      type="checkbox"
+                      checked={todo.active}
+                      onChange={() => handleToggleActive(todo.id)}
+                      className="mr-2"
+                    />
+                    {todo.text}
+                  </div>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => handleDeleteTodo(todo.id)}
+                  >
+                    Delete
+                  </button>
+                </li>
+              ))}
+            </ul>
+            {Cond ? (
+              ""
+            ) : (
+              <div className="footer mt-4">
+                <button className="btn btn-info" onClick={handleShowArchive}>
+                  {showArchive ? "Hide Archive" : "Show Archive"}
+                </button>
+                {showArchive && (
+                  <div className="archive">
+                    <h2 className="Title">Archive</h2>
+                    <ul className="list-group mt-4">
+                      {deletedTodos.map((todo) => (
+                        <li
+                          className="inputField list-group-item"
+                          key={todo.id}
+                        >
+                          {todo.text}
+                        </li>
+                      ))}
+                    </ul>
+                    <button
+                      className="btn btn-primary"
+                      onClick={handleDeleteArchive}
+                    >
+                      Delete archive
+                    </button>
+                  </div>
+                )}
+              </div>
+            )}
+          </div>
+        </div>
+        </center>
       </div>
-    </div>
+    </>
   );
 };
 
